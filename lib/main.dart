c9f999833a5475e545c5bc070ad9c5c1c7e82164
DevShivom <dev.shivompandhare@gmail.com>
2025-01-23 22:36:31 +0530

    Structure

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Api());
+}
+
+class Api extends StatelessWidget {
+  const Api({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
